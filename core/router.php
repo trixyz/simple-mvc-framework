08da1b0c422fe59bba787ad28c3020d590f61f81
trixyz <trixyz.pp@gmail.com>
2016-03-17 17:34:50 +0300

    router

--- core/router.php
+++ core/router.php
@@ -3,18 +3,48 @@
 class Router {
 
     private $routes_table = [];
+    private $params;
+
+    public function getRoutesTable(){
+
+        return $this->routes_table;
+    }
+
+    public function getParams(){
+
+        return $this->params;
+    }
 
     public function addRoute($route, $params = []){
 
         $route = preg_replace('/\//', '\\/', $route);
 
-        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<$1>[a-z-]+)', $route);
 
         $route = '/^'.$route.'$/i';
 
         $this->routes_table[$route] = $params;
 
-        print_r($this->routes_table);
+
+    }
+
+    public function matchUrl($url){
+
+        foreach ($this->routes_table as $route => $params) {
+
+            if(preg_match($route,$url,$matches)){
+                
+                foreach ($matches as $key => $match) {
+
+                    if(is_string($key)){
+                        $params[$key] = $match;
+                    }
+                }
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
     }
 
 }
--- public/index.php
+++ public/index.php
@@ -9,4 +9,13 @@ $url = $_SERVER['QUERY_STRING'];
 
 $router = new Router();
 
+//$router->addRoute('{controller}/{action}');
+$router->addRoute('',['controller'=>'home','action'=>'index']);
+$router->addRoute('posts',['controller'=>'posts','action'=>'index']);
 $router->addRoute('{controller}/{action}');
+$router->addRoute('admin/{controller}/{action}');
+
+$router->matchUrl($url);
+
+
+print_r($router->getParams());
